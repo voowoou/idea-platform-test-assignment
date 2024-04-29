a00dc2128d86ea896c6ac27c306dbe87da5a16c7
Nikolai Bik <voowoou@gmail.com>
2024-04-29 03:21:09 +0300

    Try to fix svg (4)

--- src/components/TicketsList.tsx
+++ src/components/TicketsList.tsx
@@ -54,19 +54,14 @@ export default function TicketsList({ filter }: TicketsListProps) {
                   <span className={style.stops}>
                     Пересадок: <strong>{ticket.stops}</strong>
                   </span>
-                  <svg
-                    width="6809"
-                    height="634"
-                    viewBox="0 0 6809 634"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M6576.77 366.667L6408.33 633.333L6341.67 633.333L6425.87 366.667L6247.2 366.667L6191.67 466.667L6141.67 466.667L6175 316.667L6141.67 166.667L6191.67 166.667L6247.23 266.667L6425.9 266.667L6341.67 1.01189e-05L6408.33 1.3033e-05L6576.77 266.667L6758.33 266.667C6771.59 266.667 6784.31 271.935 6793.69 281.311C6803.07 290.688 6808.33 303.406 6808.33 316.667C6808.33 329.928 6803.07 342.645 6793.69 352.022C6784.31 361.399 6771.59 366.667 6758.33 366.667L6576.77 366.667Z"
-                      fill="#909090"
-                    />
-                    <rect y="272" width="6000" height="90" fill="#909090" />
-                  </svg>
+                  <Image
+                    src="../..public/line-plane.svg"
+                    alt="Plane icon"
+                    width="130"
+                    height="30"
+                    className={style.planeIcon}
+                    priority
+                  />
                 </div>
                 <span className={style.time}>{ticket.arrival_time}</span>
               </div>
