6f6a57d2c92716866202e6cd9d816ba176910997
Nikolai Bik <voowoou@gmail.com>
2024-04-29 03:50:35 +0300

    Try to fix svg (6)

--- src/components/TicketsList.tsx
+++ src/components/TicketsList.tsx
@@ -55,7 +55,7 @@ export default function TicketsList({ filter }: TicketsListProps) {
                     Пересадок: <strong>{ticket.stops}</strong>
                   </span>
                   <Image
-                    src="./idea-platform-test-assignment/public/plane.svg"
+                    src="/idea-platform-test-assignment/plane.svg"
                     alt="Plane icon"
                     width="130"
                     height="30"
